377459f3d0db82749041b7c1040377c380ae7243
Jose Sanchez <sanchjos@outlook.com>
2019-03-28 11:42:54 -0400

    application improved

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv.load("#{Rails.root}/.env")
+#Dotenv.load("#{Rails.root}/.env")
 
 module Facebot
   class Application < Rails::Application
@@ -18,7 +18,7 @@ module Facebot
     config.load_defaults 5.2
 
     config.paths.add File.join("app", "bot"), glob: File.join("**","*.rb")
-	config.autoload_paths += Dir[Rails.root.join("app", "bot", "*")]
+    config.autoload_paths += Dir[Rails.root.join("app", "bot", "*")]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
